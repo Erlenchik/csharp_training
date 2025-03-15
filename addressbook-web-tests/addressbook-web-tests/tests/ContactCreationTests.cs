cc98b9ef86705e336ca879c435a0244627cbbebf
Ирина Зайчикова <erlenchik07@gmail.com>
2025-03-15 19:30:11 +0300

    Task 13. параметризовать тесты для создания групп и контактов

--- addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTests.cs
@@ -11,22 +11,31 @@ namespace WebAddressbookTests
     [TestFixture]
     public class ContactCreationTests : AuthTestBase
     {
-        [Test]
-        public void ContactCreationTest()
+        public static IEnumerable<ContactData> RandomGroupDataProvider()
         {
-            ContactData contact = new ContactData("Irina", "Zaichikova");      
-            
+            List<ContactData> contacts = new List<ContactData>();
+            for (int i = 0; i < 5; i++)
+            {
+                contacts.Add(new ContactData(GenerateRandomString(30), GenerateRandomString(30)));
+            }
+            return contacts;
+        }
+
+        [Test, TestCaseSource("RandomGroupDataProvider")]
+        public void ContactCreationTest(ContactData contact)
+        {
             List<ContactData> oldContacts = app.Contacts.GetContactList();
 
-            app.Contacts.Creation(contact);
+             app.Contacts.Creation(contact);
 
-            Assert.AreEqual(oldContacts.Count + 1, app.Contacts.GetContactCount());
+             Assert.AreEqual(oldContacts.Count + 1, app.Contacts.GetContactCount());
 
-            List<ContactData> newContacts = app.Contacts.GetContactList();
-            oldContacts.Add(contact);
-            oldContacts.Sort();
-            newContacts.Sort();
-            Assert.AreEqual(oldContacts, newContacts);
-        }      
+             List<ContactData> newContacts = app.Contacts.GetContactList();
+             oldContacts.Add(contact);
+             oldContacts.Sort();
+             newContacts.Sort();
+             Assert.AreEqual(oldContacts, newContacts);
+        }
     }
 }
+
